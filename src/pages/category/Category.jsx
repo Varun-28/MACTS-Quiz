e3619dae0bdc5b48faaf9b2d0a7d89a8787dcacc
Varun Verma <28varun28pp@gmail.com>
2022-04-01 00:39:18 +0530

    add: UI of home and category page

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import "./category.css";
+import { categoryData } from '../../data/categoryData';
+
 
 function Category() {
   return (
-    <div>Category</div>
+    <div className='category-container flex flex-col items-center p-8 gap-8'>
+    {
+      categoryData.map(({id, categoryName, categoryDescription, categoryImage, categoryValue}) => 
+      <div key={id} className="category-card flex items-center">
+          <div>
+            <img className='category-card-img w-48' src={categoryImage} alt="category-card" />
+          </div>
+          <div className="card-texts">
+            <h4 className="card-title">{categoryName}</h4>
+            <p className="card-subTitle">{categoryDescription}</p>
+          </div>
+      </div>
+      )
+    }
+    </div>
   )
 }
 
