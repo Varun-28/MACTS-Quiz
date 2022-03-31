51a914eef4173d234616f3817844a85c6f6c9328
Varun Verma <28varun28pp@gmail.com>
2022-03-31 15:44:44 +0530

    add: home page, navbar, footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from "../assets/quiz-logo.svg"
 import "../styles/navbar.css";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+    <Link to="/">
+      <img className='notes-logo' src={logo} alt="notes-logo" />
+    </Link>
+  </nav>
   )
 }
 
